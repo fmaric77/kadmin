a930dcd0c8df8fadc111c67a2e0e0bba473ae8fe
deniro1221 <subasicdenis771@gmail.com>
2025-06-14 00:20:59 +0200

    add helpdesk link and btn

--- src/pages/AdminBoardPage.vue
+++ src/pages/AdminBoardPage.vue
@@ -1,49 +1,60 @@
 <template>
-    <q-page class="q-pa-md">
-      <q-card>
-        <q-card-section>
-          <div class="text-h6">Admin Board</div>
-        </q-card-section>
-        <q-card-section>
-          <CategoryDropdown @categoryChanged="handleCategoryChange" />
-          <!-- Logout Button -->
-          <q-btn @click="logout" label="Odjava" color="negative" class="q-mt-md" />
-          <!-- Admin board content based on selected category -->
-          <div v-if="selectedCategory" class="q-mt-md">
-            <p>Selected Category: {{ selectedCategory }}</p>
-            <!-- Add your content here based on the selected category -->
-          </div>
-          <div class="q-mt-md text-center">
-            <img src="https://cdn-icons-png.flaticon.com/128/6024/6024190.png" width="10%" alt="admin icons" title="admin icons" />
-          </div>
-        </q-card-section>
-      </q-card>
-    </q-page>
+  <q-page class="q-pa-md">
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">Admin Board</div>
+      </q-card-section>
+      <q-card-section>
+        <CategoryDropdown @categoryChanged="handleCategoryChange" />
+        <!-- Logout Button -->
+        <q-btn
+          @click="logout"
+          label="Odjava"
+          color="negative"
+          class="q-mt-md"
+        />
+        <q-btn @click="logout" label="Pomoć" color="primary" class="q-mt-md" />
+        <!-- Admin board content based on selected category -->
+        <div v-if="selectedCategory" class="q-mt-md">
+          <p>Selected Category: {{ selectedCategory }}</p>
+          <!-- Add your content here based on the selected category -->
+        </div>
+        <div class="q-mt-md text-center">
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/6024/6024190.png"
+            width="10%"
+            alt="admin icons"
+            title="admin icons"
+          />
+        </div>
+      </q-card-section>
+    </q-card>
+  </q-page>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-import CategoryDropdown from 'src/components/CategoryDropdown.vue';
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import CategoryDropdown from "src/components/CategoryDropdown.vue";
 
-const selectedCategory = ref('');
+const selectedCategory = ref("");
 const router = useRouter();
 
 const handleCategoryChange = (category) => {
   selectedCategory.value = category;
   // Navigate to the corresponding page based on the selected category
   switch (category.value) {
-    case 'KUPAC':
-      router.push('/customers');
+    case "KUPAC":
+      router.push("/customers");
       break;
-    case 'PRODAVAC':
-      router.push('/sellers');
+    case "PRODAVAC":
+      router.push("/sellers");
       break;
-    case 'NARUDZBA':
-      router.push('/orders');
+    case "NARUDZBA":
+      router.push("/orders");
       break;
-    case 'PROIZVOD_NA_NARUDZBI':
-      router.push('/products-on-order');
+    case "PROIZVOD_NA_NARUDZBI":
+      router.push("/products-on-order");
       break;
     default:
       break;
@@ -51,7 +62,14 @@ const handleCategoryChange = (category) => {
 };
 
 const logout = () => {
-  localStorage.removeItem('authToken');
-  router.push('/');
+  localStorage.removeItem("authToken");
+  router.push("/");
+};
+const openHelp = () => {
+  // Otvaranje linka u novom tabu
+  window.open(
+    "https://ln5.sync.com/4.0/dl/4fb65ad20#4gpihw66-ty7bqhtf-bztr7e94-b6dzb73h",
+    "_blank"
+  );
 };
 </script>
